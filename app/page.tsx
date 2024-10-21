17e5f0713d472bc170e43392b487d0fba12636cd
Mahir <mahirabsar181@gmail.com>
2024-10-21 20:55:55 +0600

    feat: add header in homepage

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,17 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-      <h2 className="text-3xl text-center">Home</h2>
-    </div>
+    <section className="w-full flex-center flex-col">
+      <h1 className="head_text text-center">
+        Discover & Share
+        <br />
+        <span className="orange_gradient text-center">
+          AI-Powered Prompts
+        </span>
+      </h1>
+      <p className="desc text-center">
+        Promptopia is an AI prompting tool for modern world to discover, create and share creative prompts.
+      </p>
+    </section>
   );
 }
